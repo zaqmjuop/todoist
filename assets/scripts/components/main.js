b1bf9e377938c13a3dcf0a0d17c281e4b528b1eb
zaqmjuop <1060007045@qq.com>
2018-08-07 23:16:01 +0800

    修改main.js和index.js和datepicker.css位置

--- assets/scripts/components/main.js
+++ assets/scripts/components/main.js
@@ -1,20 +1,6 @@
 import Component from './component';
 import routerParam from './router';
-import Dom from '../dom';
 
-window.Dom = Dom;
-
-const main = () => {
-  document.addEventListener('DOMContentLoaded', () => {
-    window.addEventListener('resize', () => {
-      document.body.style.offsetHeight = `${window.innerHeight}px`;
-    });
-    window.resizeTo(window.innerWidth, window.innerHeight);
-    Component.of(routerParam);
-  });
-};
+const main = () => Component.of(routerParam);
 
 export default main;
-
-// todo mission增加state一项
-// 页面链接 改为传递 {query: 'all'} {query: 'expired'} {query: 'today'} {query: 'done'} {query: 'undone'}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,11 @@
 import main from './components/main';
+import Dom from './dom';
 
-window.now = new Date();
-main();
+document.addEventListener('DOMContentLoaded', () => {
+  window.Dom = Dom;
+  window.addEventListener('resize', () => {
+    document.body.style.offsetHeight = `${window.innerHeight}px`;
+  });
+  window.resizeTo(window.innerWidth, window.innerHeight);
+  main();
+});
