1b9ba1e78f0d418a1b4d4d2fe574187770f2eac1
zaqmjuop <1060007045@qq.com>
2018-08-09 08:21:15 +0800

    删除时钟

--- assets/scripts/components/leftMenu.js
+++ assets/scripts/components/leftMenu.js
@@ -11,31 +11,11 @@ const param = {
     };
   },
   passon: [],
-  selectors: {
-    date: '.date',
-    time: '.time',
-  },
   methods: {
     init() {
       if (this.data.inited) { return false; }
-      this.methods.fillTime();
       this.data.inited = 1;
       return this;
-    },
-    fillTime() {
-      const fill = () => {
-        // 左侧时间板
-        const now = new Date();
-        const date = utils.formatDate(now);
-        const time = utils.formatTime(now);
-        if (this.data.dateStr !== date) {
-          this.data.dateStr = date;
-          Dom.of(this.elements.date).attr('text', date);
-        }
-        Dom.of(this.elements.time).attr('text', time);
-        this.data.fillTimeOut = window.setTimeout(fill, 1000);
-      };
-      fill();
     },
   },
   created() {
