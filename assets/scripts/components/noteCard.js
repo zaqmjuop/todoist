74fdfcb0589336d6a928254092361539e39b61fc
zaqmjuop <1060007045@qq.com>
2018-08-11 20:41:25 +0800

    添加noteCard页面

---
+++ assets/scripts/components/noteCard.js
@@ -0,0 +1,17 @@
+const param = {
+  name: 'noteCard',
+  query: 'note-card',
+  url: './assets/templates/noteCard.html',
+  data() {
+    return {};
+  },
+  selectors: {
+  },
+  methods: {
+  },
+  created() {
+    console.log('noteCard create');
+  },
+};
+
+export default param;
--- assets/scripts/components/welcome.js
+++ assets/scripts/components/welcome.js
@@ -2,6 +2,7 @@ import leftMenu from './leftMenu';
 import missionCard from './missionCard';
 import missionEdit from './missionEdit';
 import quadrants from './quadrants';
+import noteCard from './noteCard';
 import Dom from '../dom';
 
 const text = {
@@ -47,6 +48,9 @@ const param = {
     } else if (this.present.action === 'quadrants') {
       quadrants.present = this.present;
       this.appendChild(quadrants, this.elements.body, 0);
+    } else if (this.present.action === 'noteCard') {
+      noteCard.present = this.present;
+      this.appendChild(noteCard, this.elements.body, 0);
     } else {
       this.present.query = this.present.query || 'all';
       missionCard.present = this.present;
